c83218572ed0fcc8b6dd09ef269e67cb8f56b3ef
leemr <think-roon@naver.com>
2022-09-29 07:23:02 +0900

    Initial commit

---
+++ src/main/java/roon/oop/objects/ObjectsApplication.java
@@ -0,0 +1,13 @@
+package roon.oop.objects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObjectsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ObjectsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/roon/oop/objects/ObjectsApplicationTests.java
@@ -0,0 +1,13 @@
+package roon.oop.objects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObjectsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
